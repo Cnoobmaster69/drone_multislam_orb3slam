e14e06a444d8941f70edde7ea3ae33f67e41728e
CarlosCortes <carlos.eduardo.cortes.cecc@gmail.com>
2026-02-11 20:48:02 -0500

    first changes for covins integration and fixed repo

---
+++ covins/covins_comm/build/CMakeFiles/covins_comm.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/home/carlos/ws_offboard_control/src/covins/covins_comm/lib/libcovins_comm.pdb"
+  "/home/carlos/ws_offboard_control/src/covins/covins_comm/lib/libcovins_comm.so"
+  "CMakeFiles/covins_comm.dir/src/covins_base/communicator_base.cpp.o"
+  "CMakeFiles/covins_comm.dir/src/covins_base/communicator_base.cpp.o.d"
+  "CMakeFiles/covins_comm.dir/src/covins_base/config_comm.cpp.o"
+  "CMakeFiles/covins_comm.dir/src/covins_base/config_comm.cpp.o.d"
+  "CMakeFiles/covins_comm.dir/src/covins_base/msgs/msg_keyframe.cpp.o"
+  "CMakeFiles/covins_comm.dir/src/covins_base/msgs/msg_keyframe.cpp.o.d"
+  "CMakeFiles/covins_comm.dir/src/covins_base/msgs/msg_landmark.cpp.o"
+  "CMakeFiles/covins_comm.dir/src/covins_base/msgs/msg_landmark.cpp.o.d"
+  "CMakeFiles/covins_comm.dir/src/covins_base/utils_base.cpp.o"
+  "CMakeFiles/covins_comm.dir/src/covins_base/utils_base.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/covins_comm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ covins/covins_comm/build/CMakeFiles/covins_comm.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for covins_comm.
+# This may be replaced when dependencies are built.
